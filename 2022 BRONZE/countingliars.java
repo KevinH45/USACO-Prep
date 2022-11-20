78da9871a21aca6dbc0d1483e793674ca95b74bc
Kevin Hwang <timmyhwang127@gmail.com>
2022-11-20 08:14:00 -0500

    liars

--- 2022 BRONZE/countingliars.java
+++ 2022 BRONZE/countingliars.java
@@ -6,6 +6,30 @@ import java.io.*;
 // c2 (-inf,4)
 public class countingliars {
     public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(System.in);
 
+        int N = scan.nextInt();
+        char[] symbols = new char[N];
+        int[] positions = new int[N];
+        int minLiars = Integer.MAX_VALUE;
+        for (int i=0; i<N; i++){
+            symbols[i] = scan.next().charAt(0);
+            positions[i] = scan.nextInt();
+        }
+
+        for (int i=0; i<N; i++){
+            int curPos = positions[i];
+            int liarCount = 0;
+            for (int j=0; j<N; j++){
+                if (symbols[j]=='L' && curPos > positions[j]){
+                    liarCount++;
+                } else if(symbols[j]=='G' && curPos < positions[j]){
+                    liarCount++;
+                }
+            }
+            minLiars = Integer.min(minLiars, liarCount);
+        }
+        System.out.println(minLiars);
+        scan.close();
     }
 }
